03ebe51098ebafd9906173c9003668e43fb19d36
otto <ottod1505@gmail.com>
2022-10-02 10:40:29 -0300

    UPLOAD Version 2.0ba subidas las variables de entorno en vercel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { CartProvider } from './context/CartContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { CartProvider } from './context/CartContext';
 import { ItemListContainer } from './components/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer';
 import { Cart } from './components/Cart';
-import Error404 from './components/Error404';
 import { OrderStatus } from './components/OrderStatus';
+import Error404 from './components/Error404';
 
 function App() {
 
